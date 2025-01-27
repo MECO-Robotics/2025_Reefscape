583caa96f79e7883e18648f6e37977225ebb7c85
Ryan <ryan.chanson2011@gmail.com>
2025-01-27 17:49:20 -0500

    drivetrian running again

--- src/main/java/frc/robot/subsystems/drive/azimuth_motor/AzimuthMotorConstants.java
+++ src/main/java/frc/robot/subsystems/drive/azimuth_motor/AzimuthMotorConstants.java
@@ -30,45 +30,45 @@ public class AzimuthMotorConstants {
   public static final AzimuthMotorHardwareConfig FRONT_LEFT_CONFIG =
       new AzimuthMotorHardwareConfig(
           new int[] {1},
-          new boolean[] {false},
+          new boolean[] {true},
           DriveConstants.steerMotorGearRatio,
           40,
           EncoderType.EXTERNAL_CANCODER,
           12,
-          Rotation2d.fromRotations(0),
+          Rotation2d.fromRotations(-0.448486328125),
           canBusName);
 
   public static final AzimuthMotorHardwareConfig FRONT_RIGHT_CONFIG =
       new AzimuthMotorHardwareConfig(
           new int[] {3},
-          new boolean[] {false},
+          new boolean[] {true},
           DriveConstants.steerMotorGearRatio,
           40,
           EncoderType.EXTERNAL_CANCODER,
           11,
-          Rotation2d.fromRotations(0),
+          Rotation2d.fromRotations(-0.1064453125),
           canBusName);
 
   public static final AzimuthMotorHardwareConfig BACK_LEFT_CONFIG =
       new AzimuthMotorHardwareConfig(
           new int[] {7},
-          new boolean[] {false},
+          new boolean[] {true},
           DriveConstants.steerMotorGearRatio,
           40,
           EncoderType.EXTERNAL_CANCODER,
           9,
-          Rotation2d.fromRotations(0),
+          Rotation2d.fromRotations(0.381591796875),
           canBusName);
 
   public static final AzimuthMotorHardwareConfig BACK_RIGHT_CONFIG =
       new AzimuthMotorHardwareConfig(
           new int[] {5},
-          new boolean[] {false},
+          new boolean[] {true},
           DriveConstants.steerMotorGearRatio,
           40,
           EncoderType.EXTERNAL_CANCODER,
           10,
-          Rotation2d.fromRotations(0),
+          Rotation2d.fromRotations(0.30908203125),
           canBusName);
 
   public static final AzimuthMotorGains FRONT_LEFT_GAINS =
