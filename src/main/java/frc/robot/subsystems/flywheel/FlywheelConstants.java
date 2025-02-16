6c00889714913c0c4839c5e8698a7f23b1695c89
Nathaniel <restofnate@gmail.com>
2025-02-16 17:20:22 -0500

    got rid of paths

--- src/main/java/frc/robot/subsystems/flywheel/FlywheelConstants.java
+++ src/main/java/frc/robot/subsystems/flywheel/FlywheelConstants.java
@@ -22,12 +22,12 @@ public class FlywheelConstants {
 
   // Coral Intake
   public static final FlywheelHardwareConfig RIGHT_CORAL_INTAKE_ROLLERS_CONFG =
-      new FlywheelHardwareConfig(new int[] {0}, new boolean[] {false}, 0, "");
+      new FlywheelHardwareConfig(new int[] {0}, new boolean[] {false}, 2.0, "");
   public static final FlywheelGains RIGHT_CORAL_INTAKE_ROLLER_GAINS =
       new FlywheelGains(0.2, 0.0, 0.0, 0.0, 0.065, 0.0, 1.0, 1.0);
 
   public static final FlywheelHardwareConfig LEFT_CORAL_INTAKE_ROLLERS_CONFG =
-      new FlywheelHardwareConfig(new int[] {0}, new boolean[] {true}, 0, "");
+      new FlywheelHardwareConfig(new int[] {0}, new boolean[] {true}, 2.0, "");
   public static final FlywheelGains LEFT_CORAL_INTAKE_ROLLER_GAINS =
       new FlywheelGains(0.2, 0.0, 0.0, 0.0, 0.065, 0.0, 1.0, 1.0);
 }
