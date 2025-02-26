df08be2f40501300618b081a20cb28535c0aada9
Javier Irizarry-Delgado <92115922+Shxl2@users.noreply.github.com>
2025-02-25 23:13:12 -0500

    Update settings

--- src/main/java/frc/robot/subsystems/position_joint/PositionJointConstants.java
+++ src/main/java/frc/robot/subsystems/position_joint/PositionJointConstants.java
@@ -31,8 +31,7 @@ public class PositionJointConstants {
       double kMinPosition,
       double kMaxPosition,
       double kTolerance,
-      double kDefaultSetpoint) {
-  }
+      double kDefaultSetpoint) {}
 
   // Position Joint Gear Ratio should be multiplied by Math.PI * 2 for rotation
   // joints to convert
@@ -46,48 +45,50 @@ public class PositionJointConstants {
       EncoderType encoderType,
       int encoderID,
       Rotation2d encoderOffset,
-      String canBus) {
-  }
+      String canBus) {}
 
-  public static final PositionJointGains EXAMPLE_GAINS = new PositionJointGains(0, 0, 0, 0.25, 0.85, 0, 0, 0, 0, -6, 0,
-      0, 0);
+  public static final PositionJointGains EXAMPLE_GAINS =
+      new PositionJointGains(0, 0, 0, 0.25, 0.85, 0, 0, 0, 0, -6, 0, 0, 0);
 
-  public static final PositionJointHardwareConfig EXAMPLE_CONFIG = new PositionJointHardwareConfig(
-      new int[] { 10 },
-      new boolean[] { true },
-      85.33333 * 2 * Math.PI,
-      40,
-      GravityType.COSINE,
-      EncoderType.EXTERNAL_CANCODER,
-      11,
-      Rotation2d.fromRotations(0.5),
-      "");
+  public static final PositionJointHardwareConfig EXAMPLE_CONFIG =
+      new PositionJointHardwareConfig(
+          new int[] {10},
+          new boolean[] {true},
+          85.33333 * 2 * Math.PI,
+          40,
+          GravityType.COSINE,
+          EncoderType.EXTERNAL_CANCODER,
+          11,
+          Rotation2d.fromRotations(0.5),
+          "");
 
   // Coral Intake
-  public static final PositionJointGains CORAL_INTAKE_ROTATION_GAINS = new PositionJointGains(20, 0.0, 0.0, 0.5, 1.0,
-      2.0, 0.0, 0.5, 0.5, 0.0, Math.PI, 0.2, 0.0);
+  public static final PositionJointGains CORAL_INTAKE_ROTATION_GAINS =
+      new PositionJointGains(20, 0.0, 0.0, 0.5, 1.0, 2.0, 0.0, 0.5, 0.5, 0.0, Math.PI, 0.2, 0.0);
 
-  public static final PositionJointHardwareConfig RIGHT_CORAL_INTAKE_RROTATION_CONFIG = new PositionJointHardwareConfig(
-      new int[] { 21 },
-      new boolean[] { false },
-      (3 * 5) * 24 / 16,
-      40,
-      GravityType.SINE,
-      EncoderType.INTERNAL,
-      -1,
-      Rotation2d.fromRotations(0),
-      "");
+  public static final PositionJointHardwareConfig RIGHT_CORAL_INTAKE_RROTATION_CONFIG =
+      new PositionJointHardwareConfig(
+          new int[] {21},
+          new boolean[] {false},
+          (3 * 5) * 24 / 16,
+          40,
+          GravityType.SINE,
+          EncoderType.INTERNAL,
+          -1,
+          Rotation2d.fromRotations(0),
+          "");
 
-  public static final PositionJointHardwareConfig LEFT_CORAL_INTAKE_RROTATION_CONFIG = new PositionJointHardwareConfig(
-      new int[] { 23 },
-      new boolean[] { true },
-      (3 * 5) * 24 / 16,
-      40,
-      GravityType.SINE,
-      EncoderType.INTERNAL,
-      -1,
-      Rotation2d.fromRotations(0),
-      "");
+  public static final PositionJointHardwareConfig LEFT_CORAL_INTAKE_RROTATION_CONFIG =
+      new PositionJointHardwareConfig(
+          new int[] {23},
+          new boolean[] {true},
+          (3 * 5) * 24 / 16,
+          40,
+          GravityType.SINE,
+          EncoderType.INTERNAL,
+          -1,
+          Rotation2d.fromRotations(0),
+          "");
 
   public static final class CORAL_ROTATION_POSITIONS {
     public static final double UP = -0.28;
