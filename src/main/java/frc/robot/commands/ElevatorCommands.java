22f99dc789e977af8612f56011ae40e5bf349084
restofnate <restofnate@gmail.com>
2025-02-28 18:59:21 -0500

    sim elevator changes

--- src/main/java/frc/robot/commands/ElevatorCommands.java
+++ src/main/java/frc/robot/commands/ElevatorCommands.java
@@ -1,15 +1,12 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.commands.position_joint.PositionJointPositionCommand;
-import frc.robot.subsystems.position_joint.PositionJoint;
 import frc.robot.util.mechanical_advantage.LoggedTunableNumber;
 
 /** A collection of commands for controlling the intake. */
 public class ElevatorCommands {
 
   /** Intake rotation preset positions. */
-  public static final class ROTATION_POSITIONS {
+  public static final class ELEVATOR_HEIGHT_PRESETS {
     public static final LoggedTunableNumber ELEVATOR_STOW =
         new LoggedTunableNumber("ElevatorPosition", 0);
     public static final LoggedTunableNumber HANDOFF =
@@ -34,10 +31,12 @@ public class ElevatorCommands {
    * new FlywheelVoltageCommand(rollerMotor, ROLLER_VOLTS.STOW));
    * }
    */
-  public static Command handOff(PositionJoint elevatorMotor) {
-    return new PositionJointPositionCommand(elevatorMotor, ROTATION_POSITIONS.HANDOFF);
-  }
-
+  /*
+   * public static Command handOff(PositionJoint elevatorMotor) {
+   * return new PositionJointPositionCommand(elevatorMotor,
+   * ROTATION_POSITIONS.HANDOFF);
+   * }
+   */
   /**
    * Deploys the intake by moving the rotation motor to the down position and setting the roller
    * motor to intake speed.
