974c4529c894980ad22933897f6c06189b5e4686
restofnate <restofnate@gmail.com>
2025-03-10 18:57:04 -0400

    nav grid changes and added in trig cameras

--- src/main/java/frc/robot/subsystems/vision/VisionConstants.java
+++ src/main/java/frc/robot/subsystems/vision/VisionConstants.java
@@ -4,6 +4,7 @@ import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.util.Units;
 
 public class VisionConstants {
   // AprilTag layout
@@ -13,6 +14,8 @@ public class VisionConstants {
   // Camera names, must match names configured on coprocessor
   public static String camera0Name = "camera_0";
   public static String camera1Name = "camera_1";
+  public static String frontTagCamera = "front_tag_camera";
+  public static String backTagCamera = "back_tag_camera";
 
   // Robot to camera transforms
   // (Not used by Limelight, configure in web UI instead)
@@ -20,6 +23,18 @@ public class VisionConstants {
       new Transform3d(0.0, 0.0, 0.0, new Rotation3d(0.0, 0.0, Math.toRadians(180.0)));
   public static Transform3d robotToCamera1 =
       new Transform3d(-0.2, 0.0, 0.2, new Rotation3d(0.0, -0.4, Math.PI));
+  public static Transform3d robotToFrontTagCamera =
+      new Transform3d(
+          Units.inchesToMeters(6 + 1 / 32),
+          Units.inchesToMeters(-2.25),
+          Units.inchesToMeters(26),
+          new Rotation3d(0.0, 0.0, Math.toRadians(0)));
+  public static Transform3d robotToBackTagCamera =
+      new Transform3d(
+          Units.inchesToMeters(-6 + 1 / 32),
+          Units.inchesToMeters(-6.25),
+          Units.inchesToMeters(26),
+          new Rotation3d(0.0, -0.4, Math.toRadians(180)));
 
   // Basic filtering thresholds
   public static double maxAmbiguity = 0.3;
