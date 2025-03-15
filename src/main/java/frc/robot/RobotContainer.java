a2d60cb94976172b2f9ee15b264c43e22ba7f94e
Javier Irizarry-Delgado <92115922+Shxl2@users.noreply.github.com>
2025-03-15 14:33:35 -0400

    Update at 'Sat Mar 15 14:33:34 EDT 2025'

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -607,14 +607,4 @@ public class RobotContainer {
     public Command getAutonomousCommand() {
         return autoChooser.get();
     }
-  
-            
-
-    
-     
-     
-     
-     
-    
-        
-    
+}
