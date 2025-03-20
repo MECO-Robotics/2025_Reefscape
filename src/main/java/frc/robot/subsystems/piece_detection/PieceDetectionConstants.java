c6726d1c097228ccae4749231ef8cc9e877ec9e8
Javier Irizarry-Delgado <92115922+Shxl2@users.noreply.github.com>
2025-03-20 09:32:31 -0400

    Update at 'Thu Mar 20 09:32:30 EDT 2025'

--- src/main/java/frc/robot/subsystems/piece_detection/PieceDetectionConstants.java
+++ src/main/java/frc/robot/subsystems/piece_detection/PieceDetectionConstants.java
@@ -6,19 +6,18 @@ import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.util.Units;
 
 public final class PieceDetectionConstants {
-  public record PieceDetectionConfig(Transform3d robotToCameraTransform) {}
+    public record PieceDetectionConfig(Transform3d robotToCameraTransform) {
+    }
 
-  public static final PieceDetectionConfig LEFT_CONFIG =
-      new PieceDetectionConfig(
-          new Transform3d(
-              new Translation3d(
-                  Units.inchesToMeters(-0.5), Units.inchesToMeters(4), Units.inchesToMeters(60)),
-              new Rotation3d(0, Units.degreesToRadians(-30), Units.degreesToRadians(90))));
+    public static final PieceDetectionConfig LEFT_CONFIG = new PieceDetectionConfig(
+            new Transform3d(
+                    new Translation3d(
+                            Units.inchesToMeters(-0.5), Units.inchesToMeters(4), Units.inchesToMeters(60)),
+                    new Rotation3d(0, Units.degreesToRadians(-30), Units.degreesToRadians(90))));
 
-  public static final PieceDetectionConfig RIGHT_CONFIG =
-      new PieceDetectionConfig(
-          new Transform3d(
-              new Translation3d(
-                  Units.inchesToMeters(-0.5), Units.inchesToMeters(-4), Units.inchesToMeters(60)),
-              new Rotation3d(0, Units.degreesToRadians(-30), Units.degreesToRadians(270))));
+    public static final PieceDetectionConfig RIGHT_CONFIG = new PieceDetectionConfig(
+            new Transform3d(
+                    new Translation3d(
+                            Units.inchesToMeters(-0.5), Units.inchesToMeters(-4), Units.inchesToMeters(60)),
+                    new Rotation3d(Units.degreesToRadians(-30), 0, Units.degreesToRadians(270))));
 }
