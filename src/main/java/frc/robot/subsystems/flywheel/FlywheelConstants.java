644906ff0e2195497978dd34f74ad6b42e75be96
Javier Irizarry-Delgado <92115922+Shxl2@users.noreply.github.com>
2025-03-20 14:32:05 -0400

    Update at 'Thu Mar 20 14:32:05 EDT 2025'

--- src/main/java/frc/robot/subsystems/flywheel/FlywheelConstants.java
+++ src/main/java/frc/robot/subsystems/flywheel/FlywheelConstants.java
@@ -1,42 +1,43 @@
 package frc.robot.subsystems.flywheel;
 
 public class FlywheelConstants {
-    public record FlywheelGains(
-            double kP,
-            double kI,
-            double kD,
-            double kS,
-            double kV,
-            double kA,
-            double kMaxAccel,
-            double kTolerance) {
-    }
+  public record FlywheelGains(
+      double kP,
+      double kI,
+      double kD,
+      double kS,
+      double kV,
+      double kA,
+      double kMaxAccel,
+      double kTolerance) {}
 
-    public record FlywheelHardwareConfig(
-            int[] canIds, boolean[] reversed, double gearRatio, int currentLimit, String canBus) {
-    }
+  public record FlywheelHardwareConfig(
+      int[] canIds, boolean[] reversed, double gearRatio, int currentLimit, String canBus) {}
 
-    public static final FlywheelHardwareConfig EXAMPLE_CONFIG = new FlywheelHardwareConfig(new int[] { 1 },
-            new boolean[] { true }, 2.0, 40, "");
+  public static final FlywheelHardwareConfig EXAMPLE_CONFIG =
+      new FlywheelHardwareConfig(new int[] {1}, new boolean[] {true}, 2.0, 40, "");
 
-    public static final FlywheelGains EXAMPLE_GAINS = new FlywheelGains(0.2, 0.0, 0.0, 0.0, 0.065, 0.0, 1.0, 1.0);
+  public static final FlywheelGains EXAMPLE_GAINS =
+      new FlywheelGains(0.2, 0.0, 0.0, 0.0, 0.065, 0.0, 1.0, 1.0);
 
-    // Coral Intake
-    public static final FlywheelHardwareConfig RIGHT_CORAL_INTAKE_ROLLERS_CONFG = new FlywheelHardwareConfig(
-            new int[] { 21 }, new boolean[] { false }, 2.0, 40, "");
-    public static final FlywheelGains RIGHT_CORAL_INTAKE_ROLLER_GAINS = new FlywheelGains(0.2, 0.0, 0.0, 0.0, 0.065,
-            0.0, 1.0, 1.0);
+  // Coral Intake
+  public static final FlywheelHardwareConfig RIGHT_CORAL_INTAKE_ROLLERS_CONFG =
+      new FlywheelHardwareConfig(new int[] {21}, new boolean[] {false}, 2.0, 40, "");
+  public static final FlywheelGains RIGHT_CORAL_INTAKE_ROLLER_GAINS =
+      new FlywheelGains(0.2, 0.0, 0.0, 0.0, 0.065, 0.0, 1.0, 1.0);
 
-    public static final FlywheelHardwareConfig LEFT_CORAL_INTAKE_ROLLERS_CONFG = new FlywheelHardwareConfig(
-            new int[] { 22 }, new boolean[] { true }, 2.0, 40, "");
-    public static final FlywheelGains LEFT_CORAL_INTAKE_ROLLER_GAINS = new FlywheelGains(0.2, 0.0, 0.0, 0.0, 0.065, 0.0,
-            1.0, 1.0);
+  public static final FlywheelHardwareConfig LEFT_CORAL_INTAKE_ROLLERS_CONFG =
+      new FlywheelHardwareConfig(new int[] {22}, new boolean[] {true}, 2.0, 40, "");
+  public static final FlywheelGains LEFT_CORAL_INTAKE_ROLLER_GAINS =
+      new FlywheelGains(0.2, 0.0, 0.0, 0.0, 0.065, 0.0, 1.0, 1.0);
 
-    public static final FlywheelGains END_EFFECTOR_GAINS = new FlywheelGains(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
-    public static final FlywheelHardwareConfig END_EFFECTOR_CONFIG = new FlywheelHardwareConfig(new int[] { 34 },
-            new boolean[] { true }, 2.0, 30, "");
+  public static final FlywheelGains END_EFFECTOR_GAINS =
+      new FlywheelGains(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
+  public static final FlywheelHardwareConfig END_EFFECTOR_CONFIG =
+      new FlywheelHardwareConfig(new int[] {34}, new boolean[] {true}, 2.0, 30, "");
 
-    public static final FlywheelGains CLIMBER_ROLLER_GAINS = new FlywheelGains(0, 0, 0, 0, 0, 0, 0, 0);
-    public static final FlywheelHardwareConfig CLIMBER_ROLLER_CONFIG = new FlywheelHardwareConfig(new int[] { 41 },
-            new boolean[] { true }, 2.0, 20, "");
+  public static final FlywheelGains CLIMBER_ROLLER_GAINS =
+      new FlywheelGains(0, 0, 0, 0, 0, 0, 0, 0);
+  public static final FlywheelHardwareConfig CLIMBER_ROLLER_CONFIG =
+      new FlywheelHardwareConfig(new int[] {41}, new boolean[] {true}, 2.0, 20, "");
 }
