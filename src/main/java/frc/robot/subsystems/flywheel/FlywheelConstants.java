b5a86c3b7df17193e5dec77fd0b31f2300c9b2ed
Javier Irizarry-Delgado <92115922+Shxl2@users.noreply.github.com>
2025-03-20 14:18:58 -0400

    Update at 'Thu Mar 20 14:18:57 EDT 2025'

--- src/main/java/frc/robot/subsystems/flywheel/FlywheelConstants.java
+++ src/main/java/frc/robot/subsystems/flywheel/FlywheelConstants.java
@@ -32,7 +32,7 @@ public class FlywheelConstants {
     public static final FlywheelGains LEFT_CORAL_INTAKE_ROLLER_GAINS = new FlywheelGains(0.2, 0.0, 0.0, 0.0, 0.065, 0.0,
             1.0, 1.0);
 
-    public static final FlywheelGains END_EFFECTOR_GAINS = new FlywheelGains(0.2, 0.0, 0.0, 0.0, 0.065, 0.0, 1.0, 1.0);
+    public static final FlywheelGains END_EFFECTOR_GAINS = new FlywheelGains(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
     public static final FlywheelHardwareConfig END_EFFECTOR_CONFIG = new FlywheelHardwareConfig(new int[] { 34 },
             new boolean[] { true }, 2.0, 30, "");
 
